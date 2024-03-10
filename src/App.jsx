f0452350b9ae6b977bfdf06378670518105409f4
JorgeRea <danireaprado@hotmail.com>
2024-03-10 14:19:39 -0600

    pagina terminada

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import AppRouter from "./routes/AppRouter"
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import 'bootstrap'
 import "boxicons"
 
 import "./styles/styles-generico.css"
